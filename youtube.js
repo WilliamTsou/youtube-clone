c05437849d0f648855a813e0201fa4d9899a833c
William Tsou <william905cm@gmail.com>
2025-12-25 23:08:45 +0800

    static website

---
+++ youtube.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  const classificationDivs = document.querySelectorAll('.classification div');
+
+  classificationDivs.forEach(div => {
+    div.addEventListener('click', function() {  
+      classificationDivs.forEach(d => d.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+});
