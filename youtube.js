6513c86f623fea493866fb88d7db4037ec19684e
William Tsou <william905cm@gmail.com>
2025-12-28 13:40:36 +0800

    finish RWD

--- youtube.js
+++ youtube.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const classificationDivs = document.querySelectorAll('.classification div');
 
   classificationDivs.forEach(div => {
-    div.addEventListener('click', function() {  
+    div.addEventListener('click', function() {
       classificationDivs.forEach(d => d.classList.remove('active'));
       this.classList.add('active');
     });
